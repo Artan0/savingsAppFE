bf222d3df22158007096108c58ba625112515473
Artan0 <artanbayram03@gmail.com>
2024-05-14 18:48:42 +0200

    design improved

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,10 @@ import CustomLayout from "../components/layout";
 import { Card, Col, Row, Button } from "antd";
 import styled from "styled-components";
 import cardImage from '../assets/images/Card 2.png'
-import savingsImage from '../assets/images/savings.jpg'
+import savingsImage from '../assets/images/savings.png'
+import saveMoneyImage from '../assets/images/save-money.png'
+import secureImage from '../assets/images/secure.png'
+import coinsImage from '../assets/images/coins.png'
 
 const StyledDivCard = styled.div`
     border: 1px solid #98989833;
@@ -11,6 +14,7 @@ const StyledDivCard = styled.div`
     padding: 10px 15px 10px 15px;
     margin: 8px;
     box-shadow: 0 3px 7px rgba(0, 0, 0, 0.1);
+    padding: 15px;
 `;
 
 const Container = styled.div`
@@ -24,12 +28,15 @@ const StyledH1 = styled.h1`
     font-weight: 500;
 `
 const StyledText = styled.p`
-    font-size:20px
+    font-size:22px
 `
 const StyledCard = styled(Card)`
     width:80%;
     box-shadow: 0 3px 7px rgba(0, 0, 0, 0.1);
-
+`
+const Description = styled.p`
+    font-size:18px;
+    font-weight:300
 `
 class Home extends Component {
     render() {
@@ -70,7 +77,7 @@ class Home extends Component {
                                                 </div>
                                                 <div className="mx-3 mt-1">
                                                     <StyledText>
-                                                        Wallet Balance: <b className="mt-2"> $34,956.50</b>
+                                                        Wallet Balance: <br /> <b className="mt-2"> $34,956.50</b>
                                                     </StyledText>
                                                 </div>
                                             </div>
@@ -106,66 +113,66 @@ class Home extends Component {
                     </div>
 
                 </Container>
+
                 <StyledContainer>
                     <div className="container py-5">
                         <Row>
+                            <Col className="text-start" span={14}>
+                                <img src={savingsImage} alt="savings" style={{ width: '90%', height: 'auto' }} />
+                            </Col>
                             <Col span={10} className="d-flex flex-column justify-content-center">
-                                <>
+                                <div>
                                     <h1>
                                         Manage your finances <br /> faster and easier
                                     </h1>
-
-                                    <StyledText>
+                                    <Description>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                                    </StyledText>
+                                    </Description>
 
-                                    <Button className="w-25" type="primary" size={'large'}>
+                                    <Button className="my-2" type="primary" size={'large'}>
                                         Register Now
                                     </Button>
-                                    <StyledText>
+                                    <Description>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                                    </StyledText>
-                                </>
-                            </Col>
-                            <Col span={14}>
-                                <img src={savingsImage} alt="savings" style={{ width: '100%', height: 'auto' }} />
+                                    </Description>
+                                </div>
                             </Col>
                         </Row>
                         <Row className="pt-5">
-                            <Col className="d-flex justify-content-center">
+                            <Col className="d-flex justify-content-center w-100">
                                 <div className="text-center w-50">
                                     <h1> Start Managing Your <br /> finances in more features</h1>
-                                    <StyledText>
+                                    <Description>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                                    </StyledText>
+                                    </Description>
                                 </div>
                             </Col>
                         </Row>
                         <Row className="d-flex justify-content-evenly py-5">
                             <Col>
                                 <StyledDivCard>
-                                    <img src="" alt="icon" />
+                                    <img src={saveMoneyImage} alt="icon" style={{ width: '50px', height: 'auto' }} />
                                     <div>
                                         <StyledText>Lorem ipsum dolor sit amet</StyledText>
-                                        <p>Lorem ipsum dolor sit amet Lorem ipsum <br />ipsum dolor sit amet</p>
+                                        <Description>Lorem ipsum dolor sit amet Lorem ipsum <br />ipsum dolor sit amet</Description>
                                     </div>
                                 </StyledDivCard>
                             </Col>
                             <Col>
                                 <StyledDivCard>
-                                    <img src="" alt="icon" />
+                                    <img src={coinsImage} alt="icon" style={{ width: '50px', height: 'auto' }} />
                                     <div>
                                         <StyledText>Lorem ipsum dolor sit amet</StyledText>
-                                        <p>Lorem ipsum dolor sit amet Lorem ipsum <br />ipsum dolor sit amet</p>
+                                        <Description>Lorem ipsum dolor sit amet Lorem ipsum <br />ipsum dolor sit amet</Description>
                                     </div>
                                 </StyledDivCard>
                             </Col>
                             <Col>
                                 <StyledDivCard>
-                                    <img src="" alt="icon" />
+                                    <img src={secureImage} alt="icon" style={{ width: '50px', height: 'auto' }} />
                                     <div>
                                         <StyledText>Lorem ipsum dolor sit amet</StyledText>
-                                        <p>Lorem ipsum dolor sit amet Lorem ipsum <br />ipsum dolor sit amet</p>
+                                        <Description>Lorem ipsum dolor sit amet Lorem ipsum <br />ipsum dolor sit amet</Description>
                                     </div>
                                 </StyledDivCard>
                             </Col>
